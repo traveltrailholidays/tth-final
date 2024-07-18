53b61da79b5c06eeb1efa5a0441802ede3b90a22
Diwakar Jha <diwakarjha554@gmail.com>
2024-07-19 05:00:03 +0530

    vercel analytics and speed insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,8 @@ import NextTopLoader from "nextjs-toploader";
 import ToasterProvider from "@/frontend/providers/ToasterProvider";
 import { Providers } from "@/components/theme/providers";
 import { GoogleTagManager } from "@next/third-parties/google";
+import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/react";
 
 const montserrat = Montserrat({
   subsets: ["latin"],
@@ -29,13 +31,15 @@ export default async function RootLayout({
 }>) {
   return (
     <html lang="en" className={montserrat.className} suppressHydrationWarning>
-      <GoogleTagManager gtmId="GTM-MP378SLG" />
       <body className="">
         <NextTopLoader color="#FF5956" height={3} showSpinner={false}/>
         <ToasterProvider />
         <Providers>
           {children}
         </Providers>
+        <GoogleTagManager gtmId="GTM-MP378SLG" />
+        <SpeedInsights/>
+        <Analytics/>
       </body>
     </html>
   );
