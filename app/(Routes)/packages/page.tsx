a79c4362dadc335e53aa0aeb1e074415ead1f44f
Diwakar Jha <diwakarjha554@gmail.com>
2024-07-18 18:52:01 +0530

    final push

--- app/(Routes)/packages/page.tsx
+++ app/(Routes)/packages/page.tsx
@@ -2,20 +2,22 @@ import ClientOnly from '@/components/features/ClientOnly';
 import AllPackages from '@/components/packages/all-packages';
 import PackagesHeroSection from '@/components/packages/packages-heroSection';
 import { IListingsParams } from '@/frontend/actions/getListings';
-import React from 'react'
+import React, { Suspense } from 'react'
 
 interface AllPackagesProps {
   searchParams: IListingsParams;
 }
 
-const page = ({searchParams}: AllPackagesProps) => {
+const page = ({ searchParams }: AllPackagesProps) => {
   return (
-    <ClientOnly>
-      <PackagesHeroSection />
-      <AllPackages
-        searchParams={searchParams}
-      />
-    </ClientOnly>
+    <Suspense>
+      <ClientOnly>
+        <PackagesHeroSection />
+        <AllPackages
+          searchParams={searchParams}
+        />
+      </ClientOnly>
+    </Suspense>
   )
 }
 
