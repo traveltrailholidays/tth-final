942647ebf003c9be2928f09ea5dcc25378099761
Diwakar Jha <diwakarjha554@gmail.com>
2024-08-19 17:38:23 +0530

    setup urls

---
+++ app/(Routes)/voucher/create-voucher/page.tsx
@@ -0,0 +1,12 @@
+import CreateVoucher from '@/components/voucher/create-voucher';
+import React from 'react';
+
+const page = () => {
+  return (
+    <>
+        <CreateVoucher />
+    </>
+  )
+}
+
+export default page;
---
+++ components/voucher/create-voucher/index.tsx
@@ -0,0 +1,15 @@
+import Container from '@/components/features/Container';
+import Section from '@/components/features/Section';
+import React from 'react';
+
+const CreateVoucher = () => {
+  return (
+    <Section className='mt-28'>
+        <Container>
+            Create voucher
+        </Container>
+    </Section>
+  )
+}
+
+export default CreateVoucher;
