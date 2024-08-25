6d3b9a7ff6fd83216b9bfe910074683a81d3e0f4
Diwakar Jha <diwakarjha554@gmail.com>
2024-08-26 03:38:55 +0530

    complete create voucher

--- components/theme/themeSwitcher.tsx
+++ components/theme/themeSwitcher.tsx
@@ -1,40 +1,47 @@
-'use client';
+"use client";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useTheme } from "next-themes"; // Assuming this is from Next.js
 
 export default function ThemeSwitcher() {
-  const { theme, setTheme } = useTheme();
-  const [isChecked, setIsChecked] = useState(theme === "dark"); // Pre-select based on initial theme
+  const [mounted, setMounted] = useState(false);
+  const { resolvedTheme, setTheme } = useTheme();
 
-  const handleCheckboxChange = () => {
-    setIsChecked(!isChecked);
-    const newTheme = theme === "light" ? "dark" : "light";
-    setTheme(newTheme);
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) {
+    return null;
+  }
+
+  const isDark = resolvedTheme === "dark";
+
+  const toggleTheme = () => {
+    setTheme(isDark ? "light" : "dark");
   };
 
   return (
-    <>
-      <label className="flex cursor-pointer select-none items-center">
-        <div className="relative">
-          <input
-            type="checkbox"
-            checked={isChecked}
-            onChange={handleCheckboxChange}
-            className="sr-only"
-          />
-          <div
-            className={`box block h-8 w-14 rounded-full ${
-              isChecked ? "bg-custom-shd" : "bg-custom-shl"
-            }`}
-          ></div>
-          <div
-            className={`absolute left-1 top-1 flex h-6 w-6 items-center justify-center rounded-full bg-custom-clp transition ${
-              isChecked ? "translate-x-full" : ""
-            }`}
-          ></div>
-        </div>
-      </label>
-    </>
+    <label className="flex cursor-pointer select-none items-center">
+      <div className="relative">
+        <input
+          type="checkbox"
+          checked={isDark}
+          onChange={toggleTheme}
+          className="sr-only"
+        />
+        <div
+          className={`box block h-8 w-14 rounded-full ${
+            isDark ? "bg-[#222222]" : "bg-border"
+          }`}
+        ></div>
+        <div
+          className={`
+            absolute left-1 top-1 flex h-6 w-6 items-center justify-center rounded-full bg-custom-clp transition 
+            ${isDark ? "translate-x-full" : ""}
+          `}
+        ></div>
+      </div>
+    </label>
   );
 }
